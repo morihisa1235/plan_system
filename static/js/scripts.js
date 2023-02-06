052c408619a7f8b923aa2dd10f783b9e75507382
itsuki1226 <i.yoshida1226@ezweb.ne.jp>
2023-02-06 14:39:41 +0900

    いろいろ修正

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,37 +10,3 @@
 function Login_alert() {
   alert("ログインしてください。")
 }
-
-//ドラッグアンドドロップ
-/***** ドラッグ開始時の処理 *****/
-function f_dragstart(event){
-  //ドラッグするデータのid名をDataTransferオブジェクトにセット
-  event.dataTransfer.setData("Text", event.target.id);
-}
-
-/***** ドラッグ要素がドロップ要素に重なっている間の処理 *****/
-function f_dragover(event){
-  //dragoverイベントをキャンセルして、ドロップ先の要素がドロップを受け付けるようにする
-  event.preventDefault();
-}
-
-/***** ドロップ時の処理 *****/
-function f_drop(event){
-  //ドラッグされたデータのid名をDataTransferオブジェクトから取得
-  var id_name = event.dataTransfer.getData("Text");
-  //id名からドラッグされた要素を取得
-  var drag_elm = document.getElementById(id_name);
-  //ドロップ先にドラッグされた要素を追加
-  event.currentTarget.appendChild(drag_elm);
-  //エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
-  event.preventDefault();
-}
-
- let win
- const openWindow = () => {
-  win = window.open("https://google.com")
- }
-
- const closeWindow = () => {
-  win.close()
- }
