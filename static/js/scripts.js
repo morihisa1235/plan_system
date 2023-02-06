6b21a0cf90f92a367b048f2ccef2b9dc8e134dfb
itsuki1226 <i.yoshida1226@ezweb.ne.jp>
2023-02-06 15:06:59 +0900

    いろいろ修正

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,3 +10,26 @@
 function Login_alert() {
   alert("ログインしてください。")
 }
+
+  function dragstart_handler(ev) {
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+  }
+
+  window.addEventListener("DOMContentLoaded", () => {
+    // Get the element by id
+    const element = document.getElementById(/^TR/);
+    // Add the ondragstart event listener
+    element.addEventListener("dragstart", dragstart_handler);
+  });
+
+  function dragover_handler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  function drop_handler(ev) {
+    ev.preventDefault();
+    // 移動された要素のidを取得して、その要素をtargetのDOMに追加する
+    const data = ev.dataTransfer.getData("text/plain");
+    ev.target.appendChild(document.getElementById(data));
+  }
